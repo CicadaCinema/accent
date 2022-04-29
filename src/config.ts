c331574ccc152750a20a4f66b0a8611ceb268f7c
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-04-29 15:28:21 +0100

    begin sveltekit migration replace site with skeleton template

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-/** base URL of the Dialect API backend */
-export const BACKEND_BASE_URL = "http://localhost:8080";
-
-/** client-side reCAPTCHA site key */
-export const RECAPTCHA_SITE_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
